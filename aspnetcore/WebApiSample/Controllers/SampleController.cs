91d2786d8612f998d54a33355e954436981f23b7
Tomislav Markovski <tmarkovski@gmail.com>
2019-07-29 15:35:58 -0400

    Add aspnetcore sample

---
+++ aspnetcore/WebApiSample/Controllers/SampleController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Streetcred.ServiceClients;
+using Streetcred.ServiceClients.Models;
+
+namespace WebApiSample.Controllers
+{
+    [ApiController]
+    public class SampleController : ControllerBase
+    {
+        private readonly IAgencyServiceClient _client;
+
+        public SampleController(IAgencyServiceClient client)
+        {
+            _client = client;
+        }
+
+        /// <summary>
+        /// Retrieve a list of registered tenant organizations
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("api/tenants")]
+        public async Task<IEnumerable<TenantContract>> Get()
+        {
+            return await _client.ListTenantsAsync();
+        }
+    }
+}
